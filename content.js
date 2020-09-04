cfb4dda11a0a4a9d220c935f273e526cdff97dc2
namdh-0976 <duong.hoai.nam@sun-asterisk.com>
2020-09-04 16:48:57 +0700

    init

---
+++ content.js
@@ -0,0 +1,6 @@
+// document.getElementById("demo");
+// alert("Hi, I got Youtube");
+console.log("Hi, I got Youtube");
+
+// const skipButton = document.getElementsByClassName('ytp-ad-preview-container countdown-next-to-thumbnail');
+// console.log(skipButton)
